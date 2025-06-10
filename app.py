e64e669fcfe003e85306099b2e0eaf278cea7a2d
JaloliddinJumanov2000 <jumanovjaloliddin7@gmail.com>
2025-06-10 20:45:04 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+
+TOKEN = getenv("BOT_TOKEN")
+
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
