c8d0d97ccd3b212218f8144b84d600b853cea710
JaloliddinJumanov2000 <jumanovjaloliddin7@gmail.com>
2025-06-11 14:02:46 +0500

    new

--- utils/db/database.py
+++ utils/db/database.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine, Column, String, Integer, BigInteger
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, session
+from sqlalchemy.testing.suite.test_reflection import users
+
 from quiz_bot.data.config import PG_USER, PG_HOST, PG_PASS, PG_PORT, PG_DB
 
 engine = create_engine(f'postgresql+psycopg2://{PG_USER}:{PG_PASS}@{PG_HOST}:{PG_PORT}/{PG_DB}')
@@ -13,3 +15,20 @@ class User(Base):
     chat_id = Column(BigInteger,unique=True)
     name = Column(String(50),nullable=False, unique=True)
     phone = Column(String(12),nullable=False, unique=True)
+
+
+if __name__ == '__main__':
+    # User obyektini yaratish va qo‘shish (xohlasang)
+    # user = User(chat_id=5937680085, fullname='Jaloliddin Jumanov')
+    # session.add(user)
+
+    # User ni chat_id orqali izlash
+    user = session.query(User).filter(User.chat_id == 5937680085).first()
+
+    if user:
+        user.fullname = 'Jumanov Jaloliddin'
+        print(f"Foydalanuvchi yangilandi: {user.fullname}")
+    else:
+        print("Foydalanuvchi topilmadi.")
+
+    session.commit()
